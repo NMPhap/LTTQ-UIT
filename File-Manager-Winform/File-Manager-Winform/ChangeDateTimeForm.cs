9319d7bfc87e2ef5924b8538e41da580c2c2af27
NMPhap <21522441@gm.uit.edu.vn>
2022-12-06 10:30:13 +0700

    <fix>: Re-add Drag and Drop function

---
+++ File-Manager-Winform/File-Manager-Winform/ChangeDateTimeForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Manager_Winform
+{
+    public partial class ChangeDateTimeForm : Form
+    {
+        public DateTime dateTime;
+        public ChangeDateTimeForm(DateTime dt)
+        {
+            dateTime = dt;
+            InitializeComponent();
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Visible = false;
+        }
+
+        private void ChangeDateTimeForm_Load(object sender, EventArgs e)
+        {
+            DatePicker.Text = dateTime.ToShortDateString();
+            TImePicker.Text = dateTime.ToLongTimeString();
+        }
+    }
+}
